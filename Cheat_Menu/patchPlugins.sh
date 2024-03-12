c2f8b0cfa566c720212c71244b0d7defe002bf0d
m5kro <44246711+m5kro@users.noreply.github.com>
2024-03-12 00:44:57 -0700

    fix patchPlugins.sh

--- Cheat_Menu/patchPlugins.sh
+++ Cheat_Menu/patchPlugins.sh
@@ -5,11 +5,37 @@
 echo "Searching for and patching RPGM Plugins.js"
 PATTERN='/];/i ,{"name":"Cheat_Menu","status":true,"description":"","parameters":{}}'
 if [ -f "www/js/plugins.js" ]; then
-	cp -v www/js/plugins.js www/js/plugins.js~ 2>/dev/null &&
-		sed -i "$PATTERN" www/js/plugins.js
+    file_path="www/js/plugins.js"
 elif [ -f "js/plugins.js" ]; then
-	cp -v js/plugins.js js/plugins.js~ 2>/dev/null &&
-		sed -i "$PATTERN" js/plugins.js
+    file_path="js/plugins.js"
 else
     echo "No RPGM installation found."
+fi
+
+
+# Find the line number of the last occurrence of "];"
+last_occurrence=$(grep -n '];' "$file_path" | tail -n 1 | cut -d ':' -f 1)
+
+# If there is an occurrence of "];"
+if [ -n "$last_occurrence" ]; then
+    # Remove the last occurrence of "];"
+    sed -i "${last_occurrence}d" "$file_path"
+
+    # Check if the last character is a comma
+    last_char=$(tail -n 1 "$file_path" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+    if [ "${last_char: -1}" = "," ]; then
+        # Comma already present, no need to add one
+        comma_needed=false
+    else
+        # Comma not present, add one
+        comma_needed=true
+    fi
+
+    # If a comma is needed, add it
+    if $comma_needed; then
+        sed -i '$s/$/,/' "$file_path"
+    fi
+
+    # Add the plugin
+    echo '{"name":"Cheat_Menu","status":true,"description":"","parameters":{}}];' >> "$file_path"
 fi
