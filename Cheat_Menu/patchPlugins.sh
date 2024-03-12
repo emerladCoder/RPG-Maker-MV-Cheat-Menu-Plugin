23865f71b1f32f194d7f21a22d71e0c4ccba77eb
m5kro <44246711+m5kro@users.noreply.github.com>
2024-03-12 01:14:00 -0700

    remove unneeded comma check

--- Cheat_Menu/patchPlugins.sh
+++ Cheat_Menu/patchPlugins.sh
@@ -20,22 +20,6 @@ last_occurrence=$(grep -n '];' "$file_path" | tail -n 1 | cut -d ':' -f 1)
 if [ -n "$last_occurrence" ]; then
     # Remove the last occurrence of "];"
     sed -i "${last_occurrence}d" "$file_path"
-
-    # Check if the last character is a comma
-    last_char=$(tail -n 1 "$file_path" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-    if [ "${last_char: -1}" = "," ]; then
-        # Comma already present, no need to add one
-        comma_needed=false
-    else
-        # Comma not present, add one
-        comma_needed=true
-    fi
-
-    # If a comma is needed, add it
-    if $comma_needed; then
-        sed -i '$s/$/,/' "$file_path"
-    fi
-
     # Add the plugin
     echo '{"name":"Cheat_Menu","status":true,"description":"","parameters":{}}];' >> "$file_path"
 fi
